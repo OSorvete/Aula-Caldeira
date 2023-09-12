51643735bec123e45a74a641f689ecd53a69d49b
OSorvete <136733778+OSorvete@users.noreply.github.com>
2023-09-12 16:35:51 -0300

    adicionando mensagem de bug do scanner\

---
+++ aula02/src/aula02/src/ex1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args){
+        double number1;
+        double number2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Me dê dois números.");
+        number1 = scanner.nextDouble();
+        number2 = scanner.nextDouble();
+        scanner.nextLine();  // bug que ocorre no scanner, assim que resolve 
+        System.out.println("Qual operacão você deseja realizar?");
+        System.out.println("Multiplicacão - Digite 1");
+        System.out.println("Divisão - Digite 2");
+        System.out.println("Subtracão - Digite 3");
+        System.out.println("Adicão - Digite 4");
+        System.out.println("Cancelar operacão - Digite n");
+        String option1 = scanner.nextLine();
+
+        switch(option1){
+            case "1" -> System.out.println("O resultado é: "  + number1 * number2);
+            case "2" -> System.out.println("O resultado é: " + number1/number2);
+            case "3" -> System.out.println("O resultado é: "  + (number1 - number2));
+            case "4" -> System.out.println ("O resultado é: " + (number1 + number2));
+            case "n" -> System.out.println ("Operacão CANCELADA.");
+        }
+    }
+}
