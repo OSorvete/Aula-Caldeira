f86c0de645b4e845aa48d14d738dcada455610ea
OSorvete <136733778+OSorvete@users.noreply.github.com>
2023-09-19 19:02:17 -0300

    Delete Retangulo.java

--- Retangulo.java
+++
@@ -1,9 +0,0 @@
-public class Retangulo extends FormaGeometrica{
-
-    private double base;
-    private double altura;
-    public double calcularArea() {
-        double area = base * altura;
-    }
-
-}
