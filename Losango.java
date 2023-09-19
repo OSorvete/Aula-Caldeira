5146dfa3dcb6d55a894da3278691385332aa8008
OSorvete <136733778+OSorvete@users.noreply.github.com>
2023-09-19 19:02:01 -0300

    Delete Losango.java

--- Losango.java
+++
@@ -1,9 +0,0 @@
-public class Losango extends FormaGeometrica{
-
-    private double diagonalMaior;
-    private double diagonalMenor;
-
-    public double calcularArea() {
-        return (diagonalMaior * diagonalMenor)/2;
-    }
-}
