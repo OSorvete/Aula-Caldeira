69678aa5f37e02145769b2197e6aecaf002ebb7d
OSorvete <136733778+OSorvete@users.noreply.github.com>
2023-09-19 19:02:51 -0300

    Delete Moto.java

--- Moto.java
+++
@@ -1,8 +0,0 @@
-public class Moto extends Veiculo{
-    double taxaPorQuilometro = 0.15;
-    @Override
-    public void calcularCustoViagem(int distancia) {
-        double custo = taxaPorQuilometro * distancia;
-        System.out.println("Você deve pagar " + custo + "ao motorista");
-    }
-}
